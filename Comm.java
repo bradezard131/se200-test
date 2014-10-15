0e73a515f3297d92a910413156627a671da3daac
bradezard131 <brad.ezard@hotmail.com.au>
2014-10-15 21:04:20 +0800

    Initial Commit

---
+++ Comm.java
@@ -0,0 +1,17 @@
+/** Comm.java
+ * 
+ * Author: Bradley Ezard
+ * Info: Mock of the 'provided' class for the Software Engineering 200
+ *       assignment run in 2014 by Dave Cooper.
+ *       Prints the message to the terminal.
+ */
+
+public abstract class Comm {
+    public Comm() {}
+
+    public void send(String message) {
+	System.out.println("Sent: " + message);
+    }
+
+    protected abstract void receive(String message);
+}
